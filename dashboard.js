534c686c66161dee94f1c899a54273c02744f148
William Klos <wjklos@gmail.com>
2016-11-12 12:41:09 -0500

    Removed demo files.

--- dashboard.js
+++
@@ -1,7 +0,0 @@
-// Get a reference to the database service
-var database = firebase.database();
-
-return firebase.database().ref('/warm-food-inventory').once('value').then(function(snapshot) {
-  var productname = snapshot.val().username;
-  // ...
-});
